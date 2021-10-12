68aa380bdc0a2a686fb059b5eb7fe6e9ef5f5838
eslamfaisal <eslam.faisal.ef@gmail.com>
2021-10-13 00:36:32 +0200

    add float button to home

--- lib/screens/home/view/home_screen.dart
+++ lib/screens/home/view/home_screen.dart
@@ -1,26 +1,28 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
-import 'package:alekhlas_teachers/screens/home/model/stattistics_model.dart';
+import 'package:alekhlas_teachers/screens/base_screen.dart';
 import 'package:alekhlas_teachers/screens/home/viewmodel/home_view_model.dart';
-import 'package:alekhlas_teachers/screens/home/widgets/card.dart';
-
-import '../../base_screen.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return BaseScreen<HomeViewModel>(
       onModelReady: (viewModel) {},
       builder: (context, viewModel, _) => Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-              ],
+        body: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+
+            },
+            child: Icon(Icons.add),
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [],
+                ),
+              ),
             ),
           ),
         ),
